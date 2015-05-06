c0ea9004a4c60cdd76a7df5372e7c6f626a72889
Michal Pravda <michal.pravda@gmail.com>
2015-05-06 08:36:24 +0200

    added 'ignored file'

---
+++ config/env/development.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+    db: {
+        superUserDbConnectString: '"sys/heslo@ruian_ldev as sysdba"',
+        peteUserDbConnectString:  '"pete_test/pete_test@ruian_ldev"'
+    },
+};
