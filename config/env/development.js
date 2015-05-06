c8392b7ba4c3998893cebe5a3d3466e80c700ba8
Michal Pravda <michal.pravda@gmail.com>
2015-05-06 09:08:00 +0200

    Revert "added 'ignored file'"
    This reverts commit c0ea9004a4c60cdd76a7df5372e7c6f626a72889.

--- config/env/development.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-    db: {
-        superUserDbConnectString: '"sys/heslo@ruian_ldev as sysdba"',
-        peteUserDbConnectString:  '"pete_test/pete_test@ruian_ldev"'
-    },
-};
